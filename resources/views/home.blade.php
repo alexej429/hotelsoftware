d92e1a2e48f9c822888411c0acf5ddbf74e0eea7
Alexej Fromm <alexej.fromm@gmx.de>
2023-11-01 20:34:49 +0100

    background image for home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+
+        <link rel="stylesheet" href="/custom.css"/>
+
+
+    </head>
+
+        
+    <body>
+        <div class="wrapper">
+
+        <div class="header">
+            <a href="/umwelt">Umweltdaten</a>
+            <a href="/">Autobuchung</a>
+            <a href="/">Login</a>
+        </div>
+
+        <div style="background-image: url(hotel-6862159_1920.jpg)">
+            Test
+        </div>
+        {{-- <img src="/hotel-6862159_1920.jpg"/> --}}
+
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::resource('umwelt', UmweltController::class);
+Route::get('/', function() {
+    return view("home");
+});
